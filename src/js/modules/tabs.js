2a3a205346c91ea5c240e82d3f1c053636831b53
Vladislav Surin <vlad_surin@mail.ru>
2023-03-19 22:19:09 +0300

    Implemented functionality for tabs

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const tabs = (headerSelector, tabsSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),
+        tabs = document.querySelectorAll(tabsSelector),
+        content =  document.querySelectorAll(contentSelector);
+
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    function showTabContent(i = 0) {
+        content[i].style.display = 'block';
+        tabs[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if (target && target.closest(tabsSelector)) {
+            tabs.forEach((item, i) => {
+                if (target.closest(tabsSelector) === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+};
+
+export default tabs;
